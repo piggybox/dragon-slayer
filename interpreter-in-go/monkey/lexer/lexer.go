b3928d3f4c6ac7b2a00a04f2516e5bad7d2d4e78
Sky Yin <sky.yin@gmail.com>
2023-07-09 21:53:12 -0700

    adjust folder structure

--- interpreter-in-go/monkey/lexer/lexer.go
+++ interpreter-in-go/monkey/lexer/lexer.go
@@ -55,8 +55,27 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 
 	l.readChar()
 	return tok
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
 }
